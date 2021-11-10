6146c97dfdee0d23352d18150ad753d42cf5ff2a
Bradley Grainger <bgrainger@gmail.com>
2021-11-09 23:01:51 -0800

    Use a record.

--- api/Dtos/UserFeedItem.cs
+++ api/Dtos/UserFeedItem.cs
@@ -1,19 +1,3 @@
 ﻿namespace FaithlifeReader.Functions.Dtos;
 
-public sealed class UserFeedItem
-{
-	public UserFeedItem(string date, string relativeDate, Uri url, string title, string details)
-	{
-		Date = date;
-		RelativeDate = relativeDate;
-		Url = url;
-		Title = title;
-		Details = details;
-	}
-
-	public string Date { get; }
-	public string RelativeDate { get; }
-	public Uri Url { get; }
-	public string Title { get; }
-	public string Details { get; }
-}
+public sealed record UserFeedItem(string Date, string RelativeDate, Uri Url, string Title, string Details);
